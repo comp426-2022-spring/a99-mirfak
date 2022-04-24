e82c4e7bf37b854ac56692774ad3ba804d2a2627
cgoldman123 <cgoldman@live.unc.edu>
2022-04-24 13:43:54 -0400

    added the national data to the side

--- template/src/mean.js
+++ template/src/mean.js
@@ -1,6 +1,6 @@
-export function mean() {
-    const api_url = "https://api.purpleair.com/v1/groups/954/members?fields=latitude%2Clongitude%2Cpm2.5_cf_1&api_key=6690E52E-B5BF-11EC-B330-42010A800004";
 
+//export function mean() {
+/*     const api_url = "https://api.purpleair.com/v1/groups/954/members?fields=latitude%2Clongitude%2Cpm2.5_cf_1&api_key=6690E52E-B5BF-11EC-B330-42010A800004";
     const response = await fetch(api_url);
     var dataJS = await response.json();
 
@@ -22,5 +22,24 @@ export function mean() {
     mean_25 = mean_25 / data.length;
     // mean_1 = mean_1 / data.length;
     // mean_10 = mean_10 / data.length;
-    return mean_25.toString;
-}
+    return mean_25.toString; */
+
+    const http = require('https');
+    const fs = require('fs');
+    const { Console } = require('console');
+
+
+    const file = fs.createWriteStream("national.csv");
+    const request = http.get("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv", function(response) {
+    response.pipe(file);
+});
+
+var data = fs.readFileSync('national.csv')
+    .toString() // convert Buffer to string
+    .split('\n') // split string to lines
+    .map(e => e.trim()) // remove white spaces for each line
+    .map(e => e.split(',').map(e => e.trim())); // split each line to array
+
+document.getElementById("date").innerHTML = data[data.length-1][0]
+document.getElementById("cases").innerHTML = data[data.length-1][1]
+document.getElementById("deaths").innerHTML = data[data.length-1][2]
