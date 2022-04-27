3b86ceea89cd116749bcaafc6ceee2651d870ed4
rvenkatraman02 <rvenka@ad.unc.edu>
2022-04-27 14:29:12 -0400

    connected webapp with server file

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const morgan = require('morgan')
 const fs = require('fs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-//app.use(express.static('./public'))
 
 // Get Port
 const args = require("minimist")(process.argv.slice(2));
-var port = args.port || 5000;
+var port = args.port || 5555;
 
 // Start an app server
 const server = app.listen(port, () => {
   console.log('App listening on port %PORT%'.replace('%PORT%',port));
 });
+
+app.use(express.static('./template/src'))
 
 app.get('/app/covid/:state', (req, res, next) => {
     const state = flipACoin(req.params.state) //create new function instead of flipACoin
