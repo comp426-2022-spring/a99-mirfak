ead90725d248934cc6e808b239ae2dfaa9a805fd
tarun.amasa@duke.edu <tarun.amasa@duke.edu>
2022-04-28 14:53:19 -0400

    added firebase

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8oMtNsfBn7MsmxiXDoMRwUtOk-1RtJcY",
+  authDomain: "unccoviddashbo.firebaseapp.com",
+  projectId: "unccoviddashbo",
+  storageBucket: "unccoviddashbo.appspot.com",
+  messagingSenderId: "490384167179",
+  appId: "1:490384167179:web:1adf521a2c46a110e8d20e"
+};
+
+export const auth = app.auth()
+export default app 
+
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
